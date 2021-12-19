1def513c73f3d92d8a6958f0b500faf3608bb915
Logan Woolf <loganwoolf@gmail.com>
2021-12-19 11:22:11 -0700

    completed 7-wallpaper

---
+++ 7-wallpaper.js
@@ -0,0 +1,22 @@
+function wallpaper(l, w, h) {
+  const numbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve","thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen", "twenty"]
+
+  for (let arg of arguments) {
+    if (arg === 0) {
+      return numbers[0]
+    }
+  }
+
+  const extra = 1.15
+
+  const perimeter = l + l + w + w
+  const levels = h / 0.52
+  const count = Math.ceil(perimeter * levels * extra / 10)
+
+  return numbers[count]
+}
+
+
+console.log(
+  wallpaper(7.8, 2.9, 3.29)
+)
