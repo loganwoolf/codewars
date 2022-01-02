075a71778a46f8807e6ac832eb3985957e1e0a6d
Logan Woolf <loganwoolf@gmail.com>
2022-01-01 22:42:56 -0700

    6-bookseller achieved correct output in main case

---
+++ 6-bookseller.js
@@ -0,0 +1,33 @@
+b = ["CBART 20", "CDXEF 50", "BKWRK 25", "BTSQZ 89", "DRTYM 60"]
+c = ["A", "B", "C", "W"]
+// res = "(A : 0) - (B : 114) - (C : 70) - (W : 0)"
+
+
+function stockList (listOfArt, listOfCat) {
+  let outputObj = {}
+  for (let category of listOfCat) {
+    outputObj[category] = 0
+    for (let bookStock of listOfArt) {
+      if (bookStock[0] === category) {
+        let quantity = +bookStock.split(' ')[1]
+        if (!outputObj[category]) {
+          outputObj[category] = quantity
+        } else {
+          outputObj[category] += quantity
+        }
+      }
+    }
+  }
+  let outputArr = []
+  for (let key in outputObj) {
+    let value = outputObj[key]
+    let strSegment = `(${key} : ${value})`
+    outputArr.push(strSegment)
+  }
+  
+  return outputArr.join(' - ')
+}
+console.log(
+  stockList(b, c)
+
+)
