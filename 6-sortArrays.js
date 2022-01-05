e5bb37b4de06172637f6ee8a4fab0c0a43dc4271
Logan Woolf <loganwoolf@gmail.com>
2022-01-05 01:23:45 -0700

    fix function name for submission

--- 6-sortArrays.js
+++ 6-sortArrays.js
@@ -2,7 +2,7 @@ function sortme ( items ) {
   return items.sort( ( a, b ) => a.localeCompare(b, 'en', {sensitivity: 'base'}) )
 }
 
-console.log(sortItems(["C", "d", "a", "B"]))
-console.log(sortItems(["Hello","there","I'm","fine"]))
-console.log(sortItems(["CodeWars"]))
-console.log(sortItems([]))
+console.log(sortme(["C", "d", "a", "B"]))
+console.log(sortme(["Hello","there","I'm","fine"]))
+console.log(sortme(["CodeWars"]))
+console.log(sortme([]))
