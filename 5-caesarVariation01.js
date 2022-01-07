b29a28f24533f31b81a8dadedc6de0060f6df05e
Logan Woolf <loganwoolf@gmail.com>
2022-01-06 20:26:05 -0700

    fix output from enc fn to be array of messages

--- 5-caesarVariation01.js
+++ 5-caesarVariation01.js
@@ -3,18 +3,24 @@ function movingShift ( clearText, shift ) {
   for (let i in clearText) {
     if (clearText[i] !== ' ') {
       let cryptCode = +clearText.charCodeAt(i) + +i + shift
-      console.log(cryptCode)
       cryptText += String.fromCharCode(cryptCode)
     } else if (clearText[i] === ' ') {
       cryptText += ' '
     }
   }
-  console.log(cryptText)
-  return cryptText
+
+  let outputArr = []
+  let messageLength = Math.floor(clearText.length / 4)
+  for (let i = 0; i < 5; i++) {
+    outputArr.push(cryptText.slice(i * messageLength, i * messageLength + messageLength))
+  }
+  console.log(outputArr)
+  return outputArr
 }
 
 
-function demovingShift ( cryptText, shift ) {
+function demovingShift ( cryptMessages, shift ) {
+  let cryptText = cryptMessages.join('')
   let clearText = ''
   for (let i in cryptText) {
     if (cryptText[i] !== ' ') {
@@ -24,13 +30,12 @@ function demovingShift ( cryptText, shift ) {
       clearText += ' '
     }
   }
-
+  console.log(clearText)
   return clearText
 }
 
+demovingShift(
+  movingShift('I should have known that you would have a perfect answer for me', 1), 1
 
-console.log(
-  demovingShift(
-    movingShift('I should have known that you would have a perfect answer for me', 1), 1
-  )
 )
+
