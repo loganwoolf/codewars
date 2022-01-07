33029c478a8d7784b0948a7960e6b160ef54f7af
Logan Woolf <loganwoolf@gmail.com>
2022-01-06 22:10:06 -0700

    fix message lengths, final submission

--- 5-caesarVariation01.js
+++ 5-caesarVariation01.js
@@ -1,54 +1,74 @@
 function movingShift ( clearText, shift ) {
+  // disallow negative shift
   if (shift < 0) return
+
+  // prepare encrypted text
   let cryptText = ''
 
   const alpha = /[a-zA-Z]/
+  // check if individual character is a letter
   for (let i in clearText) {
     if (alpha.test(clearText[i])) {
+      // find ascii code of letter
       const clearCode = clearText.charCodeAt(i)
-
+      // determine if capital or lowercase
       if (clearCode >= 65 && clearCode <= 90) {
+        // apply shift based on character position and fn parameter
         cryptCode = clearCode + (+i) + shift
+        // return shifted code to capital letter range
         while (cryptCode > 90) {
           cryptCode -= 26
         }
       } else if (clearCode >= 97 && clearCode <= 122) {
+        // same as above but for lowercase letters
         cryptCode = clearCode + (+i) + shift
         while (cryptCode > 122) {
           cryptCode -= 26
         }
       } 
 
+      // create encrypted string from character codes
       cryptText += String.fromCharCode(cryptCode)
     } else {
       cryptText += clearText[i]
     }
   }
 
+  // split message for 5 runners
   let outputArr = []
-  let messageLength = Math.ceil(clearText.length / 4)
+  let messageLength = Math.ceil(clearText.length / 5)
   for (let i = 0; i < 5; i++) {
     outputArr.push(cryptText.slice(i * messageLength, i * messageLength + messageLength))
   }
 
-  console.log(outputArr)
   return outputArr
 }
 
 
 function demovingShift ( cryptMessages, shift ) {
-  const alpha = /[a-zA-Z]/
+  // disallow negative shift
+  if (shift < 0) return
+
+  // build message from runners
   let cryptText = cryptMessages.join('')
+
+
+  const alpha = /[a-zA-Z]/
   let clearText = ''
-  // compute decrypted ascii code
+  // compute decrypted ascii code for each letter
   for (let i in cryptText) {
     // if the character is a letter
     if (alpha.test(cryptText[i])) {
+      // determine ascii code of each encrypted letter
       const cryptCode = cryptText.charCodeAt(i)
+
+      //decode
       let clearCode
+      // determine if capital or lowercase
       if (cryptCode >= 65 && cryptCode <= 90) {
-        // if the character is capital letter
+        // apply shift based on character position and fn parameter
         clearCode = cryptCode - (+i) - shift
+        // return code to ascii character range
         while (clearCode < 65) {
           clearCode += 26
         }
@@ -60,18 +80,19 @@ function demovingShift ( cryptMessages, shift ) {
         }
 
       }
+      //build message from decoded ascii codes
       clearText += String.fromCharCode(clearCode)
-      // if the character is not a letter
     } else {
+      // if the character is not a letter
       clearText += cryptText[i]
     }
   }
-  console.log(clearText)
+  
   return clearText
 }
 
 
-movingShift("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", 1)
+movingShift("I should have known that you would have a perfect answer for me!!!", -1)
 // demovingShift(
 // , 1)
   
