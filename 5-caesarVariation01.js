b9e050ea6b510f009f8f445422c5e44e986af5a5
Logan Woolf <loganwoolf@gmail.com>
2022-01-06 19:23:47 -0700

    second attempt at 5-caesarVariation01 adding shift

--- 5-caesarVariation01.js
+++ 5-caesarVariation01.js
@@ -1,23 +1,23 @@
-function movingShift ( clearText ) {
+function movingShift ( clearText, shift ) {
   let cryptText = ''
   for (let i in clearText) {
     if (clearText[i] !== ' ') {
-      let cryptCode = +clearText.charCodeAt(i) + +i + 1
+      let cryptCode = +clearText.charCodeAt(i) + +i + shift
       cryptText += String.fromCharCode(cryptCode)
     } else if (clearText[i] !== ' ') {
       cryptText += ' '
     }
   }
-  
+
   return cryptText
 }
 
 
-function demovingShift ( cryptText ) {
+function demovingShift ( cryptText, shift ) {
   let clearText = ''
   for (let i in cryptText) {
     if (cryptText[i] !== ' ') {
-      let clearCode = +cryptText.charCodeAt(i) - (+i) - 1
+      let clearCode = +cryptText.charCodeAt(i) - (+i) - shift
       clearText += String.fromCharCode(clearCode)
     } else if (cryptText[i] === ' ') {
       clearText += ' '
@@ -30,6 +30,6 @@ function demovingShift ( cryptText ) {
 
 console.log(
   demovingShift(
-    movingShift('hello')
+    movingShift('hello', 1), 1
   )
 )
