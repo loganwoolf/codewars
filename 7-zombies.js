06ebfbbaa3ffe887f22f70b941b14ccf5631e635
Logan Woolf <loganwoolf@gmail.com>
2022-04-14 21:52:23 -0600

    Refactor 7-zombies for airbnb styles

--- 7-zombies.js
+++ 7-zombies.js
@@ -1,18 +1,22 @@
-function zombie_shootout(totalZombies, range, ammo) {
-  let remainingZombies = totalZombies
-  while (ammo > 0) {
-    ammo -= 1
-    remainingZombies -= 1
-    if ( remainingZombies === 0 ) {
-      return `You shot all ${totalZombies} zombies.`
+function zombieShootout(totalZombies, range, ammo) {
+  const s = { totalZombies, range, ammo };
+  while (s.ammo > 0) {
+    s.remainingAmmo -= 1;
+    s.totalZombies -= 1;
+    if (s.totalZombies === 0) {
+      return `You shot all ${totalZombies} zombies.`;
     }
-    range -= 0.5
-    if ( range === 0 ) {
-      return `You shot ${totalZombies - remainingZombies} zombies before being eaten: overwhelmed.`
+    s.range -= 0.5;
+    if (s.range === 0) {
+      return `You shot ${
+        totalZombies - s.totalZombies
+      } zombies before being eaten: overwhelmed.`;
     }
   }
-  return `You shot ${totalZombies - remainingZombies} zombies before being eaten: ran out of ammo.`
+  return `You shot ${
+    totalZombies - s.totalZombies
+  } zombies before being eaten: ran out of ammo.`;
 }
 
-
-console.log(zombie_shootout(5, 2, 5))
+// eslint-disable-next-line no-console
+console.log(zombieShootout(5, 2, 5));
