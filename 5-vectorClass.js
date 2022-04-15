7e303d77a61fcd1d26ae58b4458a3d4638cfa46a
Logan Woolf <loganwoolf@gmail.com>
2022-04-14 22:25:24 -0600

    Create constructor and add method

---
+++ 5-vectorClass.js
@@ -0,0 +1,44 @@
+/*
+Create a Vector object that supports addition,
+subtraction, dot products, and norms.
+So, for example:
+
+var a = new Vector([1, 2, 3]);
+var b = new Vector([3, 4, 5]);
+var c = new Vector([5, 6, 7, 8]);
+
+a.add(b);      // should return a new Vector([4, 6, 8])
+a.subtract(b); // should return a new Vector([-2, -2, -2])
+a.dot(b);      // should return 1*3 + 2*4 + 3*5 = 26
+a.norm();      // should return sqrt(1^2 + 2^2 + 3^2) = sqrt(14)
+a.add(c);      // throws an error
+
+If you try to add, subtract, or dot two vectors with different lengths, you must throw an error!
+
+Also provide:
+
+a toString method, so that using the vectors from above,
+a.toString() === '(1,2,3)'
+an equals method, to check that two vectors that have the same components are equal
+Note: the test cases will utilize the user-provided equals method.
+*/
+
+class Vector {
+  constructor(x, y, z) {
+    this.x = x;
+    this.y = y;
+    this.z = z;
+  }
+
+  add(vec) {
+    const newX = this.x + vec.x;
+    const newY = this.y + vec.y;
+    const newZ = this.z + vec.z;
+    return new Vector(newX, newY, newZ);
+  }
+}
+
+const a = new Vector(1, 2, 3);
+const b = new Vector(2, 3, 4);
+/* eslint-disable no-console */
+console.log(a.add(b));
