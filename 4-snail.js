b2d88772ccf3b9e962559ee53b4c9ebbbe57ad70
Logan Woolf <loganwoolf@gmail.com>
2022-08-21 01:09:03 -0600

    Refactor 4-snail in JS for clarity

--- 4-snail.js
+++ 4-snail.js
@@ -1,25 +1,20 @@
 function snail(arr) {
-  // specify direction array
-  const directions = ['colInc', 'rowInc', 'colDec', 'rowDec'];
-  // find number of rows and columns
+  // determine distance pattern
   const cols = arr[0].length;
   const rows = arr.length;
-  // determine distance pattern
-  //   for 4x4 it's [4,3,3,2,2,1,1]
-  const dist = [];
   const alt = [cols, rows - 1];
+  const dist = [];
   let inc = 0;
   while (alt[inc % 2] > 0) {
     dist.push(alt[inc % 2]);
     alt[inc % 2] -= 1;
     inc += 1;
   }
-  // eslint-disable-next-line no-console
-  console.log({ dist });
-  // traverse
+
   let coord = [0, 0];
   const outputArr = [];
 
+  // define functions for each direction
   const colInc = (cycles) => {
     // eslint-disable-next-line prefer-const
     let [x, y] = coord;
@@ -27,6 +22,7 @@ function snail(arr) {
       outputArr.push(arr[y][x]);
       x += 1;
     }
+    // modify coord for next run
     coord = [x - 1, y + 1];
   };
   const rowInc = (cycles) => {
@@ -56,6 +52,8 @@ function snail(arr) {
     }
     coord = [x + 1, y + 1];
   };
+
+  // create object to call functions
   const traverse = {
     colDec,
     colInc,
@@ -63,9 +61,14 @@ function snail(arr) {
     rowInc,
   };
 
+  // create array to cycle through function calls in order
+  const directions = ['colInc', 'rowInc', 'colDec', 'rowDec'];
+
+  // call appropriate function for each run
   for (let i = 0; i < dist.length; i++) {
     traverse[directions[i % 4]](dist[i]);
   }
+
   return outputArr;
 }
 
