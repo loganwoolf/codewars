c58cd8272567cb11fd79d2826deb4d81cbb6e0c2
Logan Woolf <loganwoolf@gmail.com>
2022-09-04 10:22:42 -0600

    Encode cipher and test

--- 3-railFenceCipher.js
+++ 3-railFenceCipher.js
@@ -1,18 +1,10 @@
-function encodeRailFenceCipher(string, numberRails) {
-  // code
-}
-
-function decodeRailFenceCipher(string, numberRails) {
-  // code
-}
-
 function splitToRails(string, numberRails) {
   const outputArr = [];
   for (let i = 0; i < numberRails; i++) {
     outputArr.push([]);
   }
+  const cycle = 2 * numberRails - 2 || 1;
   for (let i = 0; i < string.length; i++) {
-    const cycle = 2 * numberRails - 2 || 1;
     const order = i % cycle;
     let row = order;
     if (order > cycle / 2) {
@@ -24,11 +16,19 @@ function splitToRails(string, numberRails) {
   return outputArr;
 }
 
-function formatRails(rails) {}
+function encodeRailFenceCipher(string, numberRails) {
+  return splitToRails(string, numberRails).reduce(
+    (acc, item) => acc.concat(item.join('')),
+    '',
+  );
+}
+
+function decodeRailFenceCipher(string, numberRails) {
+  // code
+}
 
 module.exports = {
   encodeRailFenceCipher,
   splitToRails,
-  formatRails,
   decodeRailFenceCipher,
 };
--- 3-railFenceCipher.test.js
+++ 3-railFenceCipher.test.js
@@ -1,12 +1,27 @@
 const {
   encodeRailFenceCipher,
   splitToRails,
-  formatRails,
   decodeRailFenceCipher,
 } = require('./3-railFenceCipher');
 
-describe('Split to rails', () => {
+describe('Split to rails function', () => {
   const string = '01234567012345670';
+
+  describe('Number of rails', () => {
+    test('is correct for 1 rail', () => {
+      const rails = 1;
+      expect(splitToRails(string, rails)).toHaveLength(rails);
+    });
+    test('is correct for 2 rails', () => {
+      const rails = 2;
+      expect(splitToRails(string, rails)).toHaveLength(rails);
+    });
+    test('is correct for 6 rails', () => {
+      const rails = 6;
+      expect(splitToRails(string, rails)).toHaveLength(rails);
+    });
+  });
+
   describe('First rail is:', () => {
     test('same as original with 1 rail', () => {
       const rails = 1;
@@ -44,7 +59,6 @@ describe('Split to rails', () => {
         '0',
       ]);
     });
-
     test('every 4th letter with 3 rails', () => {
       const rails = 3;
       expect(splitToRails(string, rails)[0]).toStrictEqual([
@@ -55,12 +69,10 @@ describe('Split to rails', () => {
         '0',
       ]);
     });
-
     test('every 8th letter with 5 rails', () => {
       const rails = 5;
       expect(splitToRails(string, rails)[0]).toStrictEqual(['0', '0', '0']);
     });
-
     test('every 14th letter with 8 rails', () => {
       const rails = 8;
       expect(splitToRails(string, rails)[0]).toStrictEqual(['0', '6']);
@@ -110,5 +122,21 @@ describe('Split to rails', () => {
       const rails = 8;
       expect(splitToRails(string, rails)[1]).toStrictEqual(['1', '5', '7']);
     });
+  });
+});
+
+describe('Encode function', () => {
+  const string = '0123456701234567';
+  test(`Correctly encodes string using 1 rail`, () => {
+    expect(encodeRailFenceCipher(string, 1)).toBe('0123456701234567');
+  });
+  test(`Correctly encodes string using 2 rails`, () => {
+    expect(encodeRailFenceCipher(string, 2)).toBe('0246024613571357');
+  });
+  test(`Correctly encodes string using 3 rails`, () => {
+    expect(encodeRailFenceCipher(string, 3)).toBe('0404135713572626');
+  });
+  test(`Correctly encodes string using too many rails`, () => {
+    expect(encodeRailFenceCipher(string, 24)).toBe('0123456701234567');
   });
 });
