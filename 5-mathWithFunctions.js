e065a22a3879f5a0289ba66b6bc867e6679751f6
Logan Woolf <loganwoolf@gmail.com>
2022-11-05 16:15:34 -0600

    Complete mathWithFunctions in JS

---
+++ 5-mathWithFunctions.js
@@ -0,0 +1,79 @@
+const operation = {
+  '+': plus,
+  '-': minus,
+  '*': times,
+  '/': dividedBy,
+};
+
+const check = (a, num) => (a?.op ? operation[a.op](a.num, num) : { num });
+
+const zero = (a) => {
+  const num = 0;
+  return check(a, num);
+};
+const one = (a) => {
+  const num = 1;
+  return check(a, num);
+};
+const two = (a) => {
+  const num = 2;
+  return check(a, num);
+};
+const three = (a) => {
+  const num = 3;
+  return check(a, num);
+};
+const four = (a) => {
+  const num = 4;
+  return check(a, num);
+};
+const five = (a) => {
+  const num = 5;
+  return check(a, num);
+};
+const six = (a) => {
+  const num = 6;
+  return check(a, num);
+};
+const seven = (a) => {
+  const num = 7;
+  return check(a, num);
+};
+const eight = (a) => {
+  const num = 8;
+  return check(a, num);
+};
+const nine = (a) => {
+  const num = 9;
+  return check(a, num);
+};
+
+function plus(a, b) {
+  return a && b ? a + b : { ...a, op: '+' };
+}
+function minus(a, b) {
+  return a && b ? a - b : { ...a, op: '+' };
+}
+function times(a, b) {
+  return a && b ? a * b : { ...a, op: '+' };
+}
+function dividedBy(a, b) {
+  return a && b ? Math.floor(a / b) : { ...a, op: '+' };
+}
+
+module.exports = {
+  zero,
+  one,
+  two,
+  three,
+  four,
+  five,
+  six,
+  seven,
+  eight,
+  nine,
+  plus,
+  minus,
+  times,
+  dividedBy,
+};
---
+++ 5-mathWithFunctions.test.js
@@ -0,0 +1,33 @@
+/* eslint-disable no-unused-vars */
+const {
+  zero,
+  one,
+  two,
+  three,
+  four,
+  five,
+  six,
+  seven,
+  eight,
+  nine,
+  plus,
+  minus,
+  times,
+  dividedBy,
+} = require('./5-mathWithFunctions');
+
+describe('module mathWithFunctions', () => {
+  describe('when there is no operator yet', () => {
+    it('numbers return themselves in an object', () => {
+      expect(four()).toMatchObject({ num: 4 });
+    });
+  });
+  describe('when calling operator function for first time', () => {
+    it('returns the first number and the operation to call', () => {
+      expect(plus(four())).toMatchObject({ num: 4, op: '+' });
+    });
+  });
+  it('composes functions to do math', () => {
+    expect(four(plus(four()))).toBe(8);
+  });
+});
