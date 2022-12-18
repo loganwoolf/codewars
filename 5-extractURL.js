1b53e98ae4f369a41e623301fce5b008f7cd3c63
Logan Woolf <logan.woolf@arcticleaf.io>
2022-12-18 15:54:26 -0700

    Complete 5-extractURL in JS

---
+++ 5-extractURL.js
@@ -0,0 +1,22 @@
+const domainName = (url) =>
+  removeSubdomains(removeTLD(removeSlug(removeProtocol(url))));
+
+const removeProtocol = (url) =>
+  url.includes('://') ? url.split('://')[1] : url;
+
+const removeSlug = (url) => (url.includes('/') ? url.split('/')[0] : url);
+
+const removeTLD = (url) =>
+  url.includes('.co.')
+    ? url.slice(0, url.lastIndexOf('.co.'))
+    : url.slice(0, url.lastIndexOf('.'));
+
+const removeSubdomains = (url) => url.slice(url.lastIndexOf('.') + 1);
+
+module.exports = {
+  domainName,
+  removeProtocol,
+  removeSlug,
+  removeTLD,
+  removeSubdomains,
+};
