1b4bef4ace327d9aed3a276362b4d9c2164dfde3
Logan Woolf <loganwoolf@gmail.com>
2023-09-14 23:03:00 -0600

    Formatting

--- 7-people-in-bus.js
+++ 7-people-in-bus.js
@@ -1,8 +1,8 @@
 const number = (stops) => {
   return stops.reduce((acc, stop) => {
-    const [on, off] = stop
-    return acc + (on - off)
-  }, 0)
+    const [on, off] = stop;
+    return acc + (on - off);
+  }, 0);
 };
 
 module.exports = { number };
--- 7-people-in-bus.test.js
+++ 7-people-in-bus.test.js
@@ -2,13 +2,28 @@ const { number } = require('./7-people-in-bus');
 
 describe('Function computeNumber', () => {
   it('should return correct number with one stop', () => {
-    expect(number([[2,2]])).toEqual(0);
-    expect(number([[2,3]])).toEqual(-1);
-    expect(number([[4,3]])).toEqual(1);
+    expect(number([[2, 2]])).toEqual(0);
+    expect(number([[2, 3]])).toEqual(-1);
+    expect(number([[4, 3]])).toEqual(1);
   });
   it('should return correct number with two stops', () => {
-    expect(number([[2,2], [3,3]])).toEqual(0);
-    expect(number([[2,3], [3,3]])).toEqual(-1);
-    expect(number([[4,3], [3,3]])).toEqual(1);
+    expect(
+      number([
+        [2, 2],
+        [3, 3],
+      ]),
+    ).toEqual(0);
+    expect(
+      number([
+        [2, 3],
+        [3, 3],
+      ]),
+    ).toEqual(-1);
+    expect(
+      number([
+        [4, 3],
+        [3, 3],
+      ]),
+    ).toEqual(1);
   });
 });
